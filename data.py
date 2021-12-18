c1f6bf72529fdeff06dc139211e4c428e3494cf4
NAoHR <najmim625@gmail.com>
2021-12-18 15:06:08 +0700

    all needed data prototype

---
+++ data.py
@@ -0,0 +1,95 @@
+allData = [
+    {
+        "yearType" : "current",
+        "sourceDataLink" : [
+            {
+                "api" : "https://ppdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-4.json",
+                "vocType" : "kgsp"
+            },
+            {
+                "api" : "https://ppdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-8.json",
+                "vocType" : "ttl"
+            },
+            {
+                "api" : "https://ppdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-14.json",
+                "vocType" : "tflm"
+            },
+            {
+                "api" : "https://ppdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-20.json",
+                "vocType" : "tmpo"
+            },
+            {
+                "api" : "https://ppdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-71.json",
+                "vocType" : "sija"
+            },
+            {
+                "api" : "https://ppdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-202.json",
+                "vocType" : "tedk"
+            }
+        ]
+    },
+    {
+        "yearType" : "2020",
+        "sourceDataLink" : [
+            {
+                "api" : "https://ppadb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-4.json",
+                "vocType" : "kgsp"
+            },
+            {
+                "api" : "https://ppadb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-8.json",
+                "vocType" : "ttl"
+            },
+            {
+                "api" : "https://ppadb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-14.json",
+                "vocType" : "tflm"
+            },
+            {
+                "api" : "https://ppadb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-20.json",
+                "vocType" : "tmpo"
+            },
+            {
+                "api" : "https://papdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-71.json",
+                "vocType" : "sija"
+            },
+            {
+                "api" : "https://ppadb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-202.json",
+                "vocType" : "tedk"
+            }
+        ]
+    }
+]
+
+#  JUNK
+kgsp2021Url = "https://ppdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-4.json"
+ttl2021Url = "https://ppdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-8.json"
+tflm2021Url = "https://ppdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-14.json"
+tmpo2021Url = "https://ppdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-20.json"
+sija2021Url = "https://ppdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-71.json"
+tedk2021Url = "https://ppdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-202.json"
+
+listjurusan2021 = [
+    {
+        "api" : kgsp2021Url,
+        "jurusan" : "kgsp"
+    },
+    {
+        "api" : ttl2021Url,
+        "jurusan" : "ttl"
+    },
+    {
+        "api" : tflm2021Url,
+        "jurusan" : "tflm"
+    },
+    {
+        "api" : tmpo2021Url,
+        "jurusan" : "tmpo"
+    },
+    {
+        "api" : sija2021Url,
+        "jurusan" : "sija"
+    },
+    {
+        "api" : tedk2021Url,
+        "jurusan" : "tedk"
+    }
+];
