96f243f1a95948486e0506394d0abb91b9e92619
NAoHR <najmim625@gmail.com>
2021-12-18 15:51:53 +0700

    prettify data

--- data.py
+++ data.py
@@ -58,38 +58,3 @@ allData = [
         ]
     }
 ]
-
-#  JUNK
-kgsp2021Url = "https://ppdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-4.json"
-ttl2021Url = "https://ppdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-8.json"
-tflm2021Url = "https://ppdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-14.json"
-tmpo2021Url = "https://ppdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-20.json"
-sija2021Url = "https://ppdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-71.json"
-tedk2021Url = "https://ppdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-202.json"
-
-listjurusan2021 = [
-    {
-        "api" : kgsp2021Url,
-        "jurusan" : "kgsp"
-    },
-    {
-        "api" : ttl2021Url,
-        "jurusan" : "ttl"
-    },
-    {
-        "api" : tflm2021Url,
-        "jurusan" : "tflm"
-    },
-    {
-        "api" : tmpo2021Url,
-        "jurusan" : "tmpo"
-    },
-    {
-        "api" : sija2021Url,
-        "jurusan" : "sija"
-    },
-    {
-        "api" : tedk2021Url,
-        "jurusan" : "tedk"
-    }
-];
