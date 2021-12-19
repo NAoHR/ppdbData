b0dd95af748650acde1e29193ce41536d4b93a96
NAoHR <najmim625@gmail.com>
2021-12-19 10:09:54 +0700

    add 2020 real data

--- data.py
+++ data.py
@@ -32,27 +32,27 @@ allData = [
         "yearType" : "2020",
         "sourceDataLink" : [
             {
-                "api" : "https://ppadb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-4.json",
+                "api" : "https://arsip.siap-ppdb.com/2020/jakarta/seleksi/prestasi/smk/2-42010088-4.json",
                 "vocType" : "kgsp"
             },
             {
-                "api" : "https://ppadb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-8.json",
+                "api" : "https://arsip.siap-ppdb.com/2020/jakarta/seleksi/prestasi/smk/2-42010088-8.json",
                 "vocType" : "ttl"
             },
             {
-                "api" : "https://ppadb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-14.json",
+                "api" : "https://arsip.siap-ppdb.com/2020/jakarta/seleksi/prestasi/smk/2-42010088-14.json",
                 "vocType" : "tflm"
             },
             {
-                "api" : "https://ppadb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-20.json",
+                "api" : "https://arsip.siap-ppdb.com/2020/jakarta/seleksi/prestasi/smk/2-42010088-20.json",
                 "vocType" : "tmpo"
             },
             {
-                "api" : "https://papdb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-71.json",
+                "api" : "https://arsip.siap-ppdb.com/2020/jakarta/seleksi/prestasi/smk/2-42010088-71.json",
                 "vocType" : "sija"
             },
             {
-                "api" : "https://ppadb.jakarta.go.id/seleksi/prestasi/smk/1-42010088-202.json",
+                "api" : "https://arsip.siap-ppdb.com/2020/jakarta/seleksi/prestasi/smk/2-42010088-202.json",
                 "vocType" : "tedk"
             }
         ]
