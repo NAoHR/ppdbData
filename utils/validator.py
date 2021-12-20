83e71aef29fc8475c5f418574dd6c37272a02cd1
NAoHR <najmim625@gmail.com>
2021-12-21 06:18:21 +0700

    fix minor config on validator : specified returned null value as jhs or current

--- utils/validator.py
+++ utils/validator.py
@@ -25,7 +25,7 @@ How To Use:
                     return loadJson["data"]
 
             except FileNotFoundError:
-                print(f"{fileName} Not Found")
+                print(f"[x] {fileName} Not Found")
                 return False
 
     def validateOtherTag(self,wantToCheck,validType):
@@ -33,18 +33,20 @@ How To Use:
             ind = self.dataList.index(wantToCheck)
             if ind == len(self.dataList) -1:
                 if wantToCheck == "-y":
-                    print("you didn't specify year type,now using the default value 'all' ")
+                    print("[x] you didn't specify year type,now using the default value 'all' ")
                 else:
-                    print("you didn't specify data type,now using the default value 'jhs' ")
-                return False
+                    print("[x] you didn't specify data type,now using the default value 'jhs' ")
+                return "jhs" if wantToCheck == "-t" else "current"
             else:
                 if self.dataList[ind+1] in validType:
                     return self.dataList[ind+1]
-                errMessage = f"value you chose didn't pass any {'yeartype' if wantToCheck == '-y' else 'type'}, now using a default value ,{'all' if wantToCheck == '-y' else 'jhs'}"
+                errMessage = f"[x] value you chose didn't pass any {'yeartype' if wantToCheck == '-y' else 'type'}, now using a default value ,{'all' if wantToCheck == '-y' else 'jhs'}"
                 print(errMessage)
-                return False
+                return "jhs" if wantToCheck == "-t" else "current"
         except ValueError:
-            return False
+            errMessage = f"[x] you didnt specify any {wantToCheck} option,now you are using {'current' if wantToCheck == '-y' else 'jhs'} as a defailt value"
+            print(errMessage)
+            return "jhs" if wantToCheck == "-t" else "current"
     def validateArgv(self):
         if len(self.dataList) == 1 :
             self.propperUse()
@@ -56,8 +58,8 @@ How To Use:
                 if(self.dataList[1] == "-f" and len(self.dataList) >= 3):
                     toBeReturned = {
                         "data" : False,
-                        "year" : False,
-                        "type" : False
+                        "year" : "current",
+                        "type" : "jhs"
                     }
                     fileCheck = self.validateFile(self.dataList[2])
                     if fileCheck == False:
