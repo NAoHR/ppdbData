54d35d853b6e445f6c158f2a0a4a84f76c37b752
NAoHR <najmim625@gmail.com>
2021-12-21 11:29:07 +0700

    script to run: main script

---
+++ main.py
@@ -0,0 +1,19 @@
+from utils.getdata import ParseData
+from utils.validator import ValidateInput
+import sys
+
+
+if __name__ == "__main__":
+    neededInput = sys.argv
+
+    vdator = ValidateInput(neededInput)
+    data = vdator.validateArgv()
+    if data == False:
+        print("Cant Proceed Data")
+    else:
+        print()
+        parseData = ParseData(data["data"])
+        returned = parseData.mainProccess(data["year"],data["type"])
+        print(returned)
+
+    
