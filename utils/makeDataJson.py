c54897d578d22e361dbacb2aafd6b472bb4843f6
NAoHR <najmim625@gmail.com>
2022-01-03 22:38:07 +0700

    automate api: add credential informations

--- utils/makeDataJson.py
+++ utils/makeDataJson.py
@@ -1,23 +1,42 @@
-class MakeDataJson:
-    data = {
-        "smp" : {
-            "current" : {
-                "link" : "https://ppdb.jakarta.go.id/sekolah/1-smp-prestasi.json"
-            }
-        },
-        "sma" : {
-            "current" : {
-                "link" : "https://ppdb.jakarta.go.id/sekolah/1-sma-prestasi.json",
+class MakeDataJson:    
+    def __init__(self,data,schoolType):
+        self.data = {
+            "smp" : {
+                "current" : {
+                    "link" : "https://ppdb.jakarta.go.id/sekolah/1-smp-prestasi.json"
+                },
+                "ayearBefore" : {
+                    "link" : "https://arsip.siap-ppdb.com/2020/jakarta/sekolah/1-smp-prestasi.json"
+                },
+                "eachVoc" : False
             },
-        },
-        "smk" : {
-            "current" : {
-                "link" : "https://ppdb.jakarta.go.id/sekolah/1-smk-prestasi.json"
+            "sma" : {
+                "yearType" : {
+                    "current" : {
+                        "link" : "https://ppdb.jakarta.go.id/sekolah/1-sma-prestasi.json",
+                    },
+                    "ayearBefore" : {
+                        "link" : "https://arsip.siap-ppdb.com/2020/jakarta/sekolah/1-sma-prestasi.json"
+                    }
+                },
+                "eachVoc" : {
+                    "current" : "https://ppdb.jakarta.go.id/sekolah/kompetensi/1-sma-prestasi.json",
+                    "ayearBefore" : "https://arsip.siap-ppdb.com/2020/jakarta/sekolah/kompetensi/1-sma-prestasi.json"
+                }
             },
-            "ayearBefore" : {
-                "link" : "https://arsip.siap-ppdb.com/2020/jakarta/sekolah/2-smk-prestasi.json"
+            "smk" : {
+                "yearType" : {
+                    "current" : {
+                        "link" : "https://ppdb.jakarta.go.id/sekolah/1-smk-prestasi.json"
+                    },
+                    "ayearBefore" : {
+                        "link" : "https://arsip.siap-ppdb.com/2020/jakarta/sekolah/2-smk-prestasi.json"
+                    }
+                },
+                "eachVoc" : {
+                    "current" : "https://ppdb.jakarta.go.id/sekolah/kompetensi/1-smk-prestasi.json",
+                    "ayearBefore" : "https://arsip.siap-ppdb.com/2020/jakarta/sekolah/kompetensi/2-smk-prestasi.json"
+                }
             }
         }
-    }    
-    def __init__(self):
-        return 
+        self.schoolType = schoolType
