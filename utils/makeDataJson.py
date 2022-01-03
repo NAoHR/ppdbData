e2e4715c1b231c01272caccc0217b5575d994f85
NAoHR <najmim625@gmail.com>
2022-01-03 23:48:02 +0700

    proto: proto logic of get credential data

--- utils/makeDataJson.py
+++ utils/makeDataJson.py
@@ -1,41 +1,96 @@
+import requests
+
+
 class MakeDataJson:    
-    def __init__(self,data,schoolType):
+    def __init__(self):
+        self.schoolType = ""
+        self.yearType = ""
+        self.schoolCred = ""
         self.data = {
-            "smp" : {
-                "current" : {
-                    "link" : "https://ppdb.jakarta.go.id/sekolah/1-smp-prestasi.json"
+            "accepted" : {
+                "sctype" : {
+                    "1" : "smp",
+                    "2" : "sma",
+                    "3" : "smk"
                 },
-                "ayearBefore" : {
-                    "link" : "https://arsip.siap-ppdb.com/2020/jakarta/sekolah/2-smp-prestasi.json"
-                },
-                "eachVoc" : False
-            },
-            "sma" : {
-                "yearType" : {
-                    "current" : {
-                        "link" : "https://ppdb.jakarta.go.id/sekolah/1-sma-prestasi.json",
-                    },
-                    "ayearBefore" : {
-                        "link" : "https://arsip.siap-ppdb.com/2020/jakarta/sekolah/1-sma-prestasi.json"
-                    }
-                },
-                "eachVoc" : {
-                    "current" : "https://ppdb.jakarta.go.id/sekolah/kompetensi/1-sma-prestasi.json",
-                    "ayearBefore" : "https://arsip.siap-ppdb.com/2020/jakarta/sekolah/kompetensi/1-sma-prestasi.json"
+                "scYear" : {
+                    "1" : "current",
+                    "2" : "atearBefore"
                 }
             },
-            "smk" : {
-                "yearType" : {
-                    "current" : {
-                        "link" : "https://ppdb.jakarta.go.id/sekolah/1-smk-prestasi.json"
+            "school" : {
+                "smp" : {
+                    "yearType" : {
+                        "current" : {
+                            "link" : "https://ppdb.jakarta.go.id/sekolah/1-smp-prestasi.json",
+                            "prefix" : "1"
+                        },
+                        "ayearBefore" : {
+                            "link" : "https://arsip.siap-ppdb.com/2020/jakarta/sekolah/2-smp-prestasi.json",
+                            "prefix" : "2"
+                        }
                     },
-                    "ayearBefore" : {
-                        "link" : "https://arsip.siap-ppdb.com/2020/jakarta/sekolah/2-smk-prestasi.json"
+                    "eachVoc" : False
+                },
+                "sma" : {
+                    "yearType" : {
+                        "current" : {
+                            "link" : "https://ppdb.jakarta.go.id/sekolah/1-sma-prestasi.json",
+                            "prefix" : "1"
+                        },
+                        "ayearBefore" : {
+                            "link" : "https://arsip.siap-ppdb.com/2020/jakarta/sekolah/1-sma-prestasi.json",
+                            "prefix" : "2"
+                        }
+                    },
+                    "eachVoc" : {
+                        "current" : "https://ppdb.jakarta.go.id/sekolah/kompetensi/1-sma-prestasi.json",
+                        "ayearBefore" : "https://arsip.siap-ppdb.com/2020/jakarta/sekolah/kompetensi/1-sma-prestasi.json"
                     }
                 },
-                "eachVoc" : {
-                    "current" : "https://ppdb.jakarta.go.id/sekolah/kompetensi/1-smk-prestasi.json",
-                    "ayearBefore" : "https://arsip.siap-ppdb.com/2020/jakarta/sekolah/kompetensi/2-smk-prestasi.json"
+                "smk" : {
+                    "yearType" : {
+                        "current" : {
+                            "link" : "https://ppdb.jakarta.go.id/sekolah/1-smk-prestasi.json",
+                            "prefix" : "1"
+                        },
+                        "ayearBefore" : {
+                            "link" : "https://arsip.siap-ppdb.com/2020/jakarta/sekolah/2-smk-prestasi.json",
+                            "prefix" : "2"
+                        }
+                    },
+                    "eachVoc" : {
+                        "current" : "https://ppdb.jakarta.go.id/sekolah/kompetensi/1-smk-prestasi.json",
+                        "ayearBefore" : "https://arsip.siap-ppdb.com/2020/jakarta/sekolah/kompetensi/2-smk-prestasi.json"
+                    }
                 }
             }
         }
+
+    def getSchoolAndYear(self):
+        schoolType = int(input("schoolType : "))
+        if schoolType in range(1,4):
+            isThere = self.data["accepted"]["sctype"].get(str(schoolType))
+            if isThere != None:
+                self.schoolType = isThere
+                while True:
+                    year = int(input("yearType : "))
+                    notNone = self.data["accepted"]["scYear"].get(str(year))
+                    if notNone != None:
+                        self.yearType = notNone
+                        break
+                return True if self.schoolType != "" and self.yearType != "" else False
+            else:
+                return self.showSchoolType()
+        else:
+            return self.showSchoolType()
+
+    def reqToData(self):
+        r = requests.get(self.data["school"][self.schoolType]["yearType"][self.yearType]["link"])
+        print(r)
+    # def allSchoolVoc(self,schoolId,yearType):
+    #     if school
+
+a = MakeDataJson()
+a.getSchoolAndYear()
+a.reqToData()
